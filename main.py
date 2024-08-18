98c474c8c83387f113305c5edd55e0059b9ea1bb
Gabriel-Sato <gusato.1@gmail.com>
2024-08-18 20:57:29 -0300

    finished countChar, added intro, added report, end of bookbot

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 def main():
+    introduction()
   #  printbook()
-  #  countWords()
+    countWords()
     countChar()
-
+def introduction():
+    intro = f"Fun facts about {f.name}"
+    print(intro)
 def printbook():  
     print(file_contents)
     
 def countWords():
     words = file_contents.split() 
-    print(len(words))
+    print(f'This book have {len(words)} words')
 def countChar():
     import string
     alphabet = list(string.ascii_lowercase)
     lowerCase = file_contents.lower()
-    teste = "teste abcde"
-    print(alphabet)
+    for x in alphabet:
+        print(f'The character {x} appeared {lowerCase.count(x)} times')    
 main()
