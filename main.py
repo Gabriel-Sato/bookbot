3848e8cd16edfdb9bdc8e9e12916ef10ce9d7a91
Gabriel-Sato <gusato.1@gmail.com>
2024-08-18 00:07:23 -0300

    changed main.py (countChar), added gitignore, added one book

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+def main():
+  #  printbook()
+  #  countWords()
+    countChar()
+
+def printbook():  
+    print(file_contents)
+    
+def countWords():
+    words = file_contents.split() 
+    print(len(words))
+def countChar():
+    import string
+    alphabet = list(string.ascii_lowercase)
+    lowerCase = file_contents.lower()
+    teste = "teste abcde"
+    print(alphabet)
+main()
